66897203b0706aaa28ca2f95fb6d7565af6fe6ad
Олег Супер Кот Спайк <olehniki05@gmail.com>
2025-09-26 17:55:26 +0200

    added manager to login

--- home/models.py
+++ home/models.py
@@ -32,6 +32,26 @@ class Genre(models.Model):
         return self.name
     
 # this model defines the new user profile
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,name,birth_year,password=None,**extra_fields):
+        if not email: raise ValueError('email doesnt exist')
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email, birth_year=birth_year, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    def create_superuser(self,email,name,birth_year,password=None,**extra_fields):
+      extra_fields.setdefault('is_staff',True)
+      extra_fields.setdefault('is_superuser', True) 
+      if extra_fields.get('is_staff') is not True:
+          raise ValueError ('must be is istaff true')
+      if extra_fields.get('is_superuser') is not True:
+          raise ValueError ('must be is superuser true')
+ 
+      return self.create_user(email, name, birth_year, password, **extra_fields)
+    def get_by_natural_key(self, email):
+        return self.get(email=email)
 
 class MyUser(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=15)
@@ -39,9 +59,14 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     birth_year = models.IntegerField()
     date_joined = models.DateTimeField(auto_now_add=True)
 
+    #allowings
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'birth_year']
-
+    objects = MyUserManager()
 
     # дописать менеджер
     def __str__(self):
--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.home,name='home'),
     path('user_profile/', views.user_profile, name='user_profile'),
-    #path('register/', views.register_view, name='register'),
-    #path('login/',views.login_view(template_name='account/login.html'), name='login'),
-    #path('logout/',auth_views.LogoutView.as_view(), name='logout'),
-    #path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('register/', views.register_view, name='register'),
+    path('login/',views.login_view, name='login'),
+    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     
 ]
